a12636952caff71c8b13bbb51d5b31f621464114
chaospace <ichaospace@gmail.com>
2023-07-04 14:45:59 +0900

    [zustand상태 주요내용 정리]

--- test/state.test.ts
+++ test/state.test.ts
@@ -1,6 +1,5 @@
-import { StoreApi, UseBoundStore, create } from "zustand";
-import { computed } from "zustand-middleware-computed-state";
-// import { subscribeWithSelector } from "zustand/middleware";
+import { create } from "zustand";
+import computed  from "zustand-computed";
 
 type State = {
     name:string
@@ -8,54 +7,41 @@ type State = {
 }
 
 type ComputedState = {
-    nickname:() => string
+    nickname:()=>string
 }
 
-type CombinedStore = State & ComputedState;
+const computedState = (state:State):ComputedState =>  ({
+    nickname:() => state.name+'!!'
+})
+
 
 describe("zustand상태 동작테스트",()=>{
 
-    let useCombinedState:UseBoundStore<StoreApi<CombinedStore>>;
-
-    beforeAll(()=>{
-        useCombinedState = create<CombinedStore>(
-            computed<State, ComputedState>(
-                (set:any)=>({
-                    name:'chaospace',
-                    setName:(newName:string) => set({name:newName})
-                }),
-                (state:State) => {
-                    return {
-                        nickname() {
-                            console.log('nick-name-call');
-                            return state.name + '~~~'
-                        }
-                    }
-                }
-            )
-        );
-    });
+    const useCombinedState = create<State>()(
+        computed(
+            (set)=>({
+                name:'chaopace',
+                setName:(newName:string) => set({name:newName})
+            }),
+            computedState
+        )
+    );
 
 
     it("computed플러그인은 상태변경 시에만 동작한다.",()=>{
         console.log('nick-name',useCombinedState.getState().nickname());
-        useCombinedState.getState().setName('one-piece-name');
     });
 
 });
 
 
 
+// type test
+// declare const withError:<T,E>(p:Promise<T>) => Promise<[error:undefined, value:T] | [error:E, value:undefined]>;
 
-describe("set동작테스트",()=>{
-    it("add를 통해 함수등록가능",()=>{
-        const listeners = new Set();
+// declare const doSomthing:() => Promise<string>;
 
-        listeners.add((value:any)=>{
-            console.log('new-value', value);
-        });
+// const main = async() => {
+//     let [error, value] = await withError<string, number>(doSomthing());
+// }
 
-
-
-    });
-});
